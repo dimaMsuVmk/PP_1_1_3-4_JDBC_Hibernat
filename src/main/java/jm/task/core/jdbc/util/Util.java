f3480d60ed5fade0eea59c0addd961d42d66d26b
dimaMsuVmk <mega.dima.r@mail.ru>
2023-06-19 16:02:33 +0300

    HW-hibernate-fix-add_static_variable_in_Util-"sessionFactory"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,24 +15,26 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static SessionFactory sessionFactory = null;
 
-    public static void main(String[] args) {
-
-    }
     public static SessionFactory getSessionFactory() {
         Properties properties = new Properties();
         properties.put(Environment.DRIVER, DRIVER_NAME);
         properties.put(Environment.URL, URL);
         properties.put(Environment.USER, USERNAME);
         properties.put(Environment.PASS, PASSWORD);
-        properties.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
         properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-        return new Configuration()
-                .setProperties(properties)
-                .addAnnotatedClass(User.class)
-                .buildSessionFactory();
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration()
+                    .setProperties(properties)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }
+        return sessionFactory;
     }
+
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
